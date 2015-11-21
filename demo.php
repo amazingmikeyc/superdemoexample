f1277e3b458e6f4277a72b5ecc082dc8e1fa1380
Mike Congreve <mike.congreve@gmail.com>
2015-11-21 12:09:04 +0000

    Update demo.php

--- demo.php
+++ demo.php
@@ -4,13 +4,14 @@ $password = $_POST['password'];
 $username = $_POST['username'];
 
 
-$results = mysql_query("select * from tbl_logginsand_passwords WHERE username = '".$_POST['username'].'" AND password = '".$_POST['password']."'");
+$results = mysql_query("select * from tbl_logginsand_passwords WHERE usersname = 
+    '".$_POST['username'].'" AND password = '".$_POST['password']."'");
 
 
 if (!$results) {
     echo "that is not the password";
 } else {
-
+    include("log-me-in.php");
 }
 
 ?>
