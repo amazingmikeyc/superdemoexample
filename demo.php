6fe864c9cbdebaf8a424e7ef317973cbb10edc3b
Mike Congreve <mike.congreve@gmail.com>
2015-11-21 13:50:08 +0000

    Update demo.php

--- demo.php
+++ demo.php
@@ -8,8 +8,8 @@ $password = $_POST['password'];
 $username = $_POST['username'];
 
 
-$results = mysql_query("select * from tbl_logginsand_passwords WHERE usersname = 
-    '".$_POST['username'].'" AND password = '".$_POST['password']."')");
+$results = mysql_query("select * from tbl_logginsand_passwords WHERE usersname = "
+    .$_POST['username']. "' AND password = '".$_POST['password']."')");
 
 
 if (!$results) {
